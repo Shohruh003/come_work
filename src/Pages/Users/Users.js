b9c43c6ad194b158e5e8f69d72d42478eb0ca0b9
Shohruh003 <shohruhazimov0705@gmail.com>
2023-11-09 09:03:23 +0500

    Update users API

--- src/Pages/Users/Users.js
+++ src/Pages/Users/Users.js
@@ -5,37 +5,37 @@ import axios from 'axios';
 
 function Users () {
     const [users, setUsers] = useState()
-    const data = [
-      {
-        "id": 159,
-        "image": "http://faceids.tadi.uz/uploads/2023/11/07/face_event/130030_face_thumbnail_TdrhiQ.jpg",
-        "name": "Azimov Shohruh",
-        "create_date": "2023-11-07T13:00:28+00:00"
-      },
-      {
-        "id": 55,
-        "image": "http://faceids.tadi.uz/uploads/2023/11/07/face_event/084224_face_thumbnail_oUS8CK.jpg",
-        "name": "Alijon Abdurasulov",
-        "create_date": "2023-11-07T08:42:23+00:00"
-      },
-      {
-        "id": 157,
-        "image": "http://faceids.tadi.uz/uploads/2023/11/07/face_event/082231_face_thumbnail_EmVDuo.jpg",
-        "name": "Sharofiddin",
-        "create_date": "2023-11-07T08:22:28+00:00"
-      },
-      {
-        "id": 3,
-        "image": "http://faceids.tadi.uz/uploads/2023/11/07/face_event/033438_face_thumbnail_YJuIwp.jpg",
-        "name": "Erkin Narmetov",
-        "create_date": "2023-11-01T03:34:37+00:00"
-      }
-    ]
+    // const data = [
+    //   {
+    //     "id": 159,
+    //     "image": "http://faceids.tadi.uz/uploads/2023/11/07/face_event/130030_face_thumbnail_TdrhiQ.jpg",
+    //     "name": "Azimov Shohruh",
+    //     "create_date": "2023-11-07T13:00:28+00:00"
+    //   },
+    //   {
+    //     "id": 55,
+    //     "image": "http://faceids.tadi.uz/uploads/2023/11/07/face_event/084224_face_thumbnail_oUS8CK.jpg",
+    //     "name": "Alijon Abdurasulov",
+    //     "create_date": "2023-11-07T08:42:23+00:00"
+    //   },
+    //   {
+    //     "id": 157,
+    //     "image": "http://faceids.tadi.uz/uploads/2023/11/07/face_event/082231_face_thumbnail_EmVDuo.jpg",
+    //     "name": "Sharofiddin",
+    //     "create_date": "2023-11-07T08:22:28+00:00"
+    //   },
+    //   {
+    //     "id": 3,
+    //     "image": "http://faceids.tadi.uz/uploads/2023/11/07/face_event/033438_face_thumbnail_YJuIwp.jpg",
+    //     "name": "Erkin Narmetov",
+    //     "create_date": "2023-11-01T03:34:37+00:00"
+    //   }
+    // ]
 
     const fetchData = async () => {
         try {
-          // const response = await axios.get('https://www.workers.yomon-emas.uz/api/get-workers');
-          setUsers(data)
+          const response = await axios.get('https://www.workers.yomon-emas.uz/api/get-workers');
+          setUsers(response.data)
         } catch (error) {
           console.error(error);
         }
